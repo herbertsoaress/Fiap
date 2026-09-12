fe3817a2a7184ef5d05f88a12a05f5c98bd64347
herbertsoaress <hsjsoarrs55@gmail.com>
2026-09-12 19:17:39 -0300

    Adicionado U e o D do CRUD

--- PCAP/Aula_modularizacao/app.py
+++ PCAP/Aula_modularizacao/app.py
@@ -17,27 +17,47 @@ def add_lead():
 def list_leads():
     leads = control.read_leads()
     print(leads)
- 
+
+def update_lead():
+    email = input("Email do lead que deseja atualizar: ")
+    novo_status = input("Novo status: ")
+    if control.upadate_leads(email, novo_status):
+        print("Lead atualizado!")
+    else:
+        print("Lead nao encontrado.")
+
+def delete_lead():
+    email = input("Email do lead que deseja remover: ")
+    if control.delete_lead(email):
+        print("Lead removido!")
+    else:
+        print("Lead nao encontrado.")
+
 def main():
     while True:
         print("\nmini crm de leads")
         print("[1] Adicionar lead")
         print("[2] Listar lead")
+        print("[3] Atualizar lead")
+        print("[4] Remover lead")
         print("[0] Sair do programa")
- 
+
         opt = input("Escolha uma opcao:")
- 
+
         if opt == "1":
             add_lead()
         elif opt == "2":
             list_leads()
+        elif opt == "3":
+            update_lead()
+        elif opt == "4":
+            delete_lead()
         elif opt =="0":
             print("Ate mais...")
             break
         else:
             print()
- 
- 
+
+
 if __name__ == "__main__":
     main()
- 
--- PCAP/Aula_modularizacao/control.py
+++ PCAP/Aula_modularizacao/control.py
@@ -27,4 +27,23 @@ def create_lead(lead_dict):
     leads.append(lead_dict)
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
 
+#Upadate
+def upadate_leads(email, new_status):
+    leads = read_leads()
+    for lead in leads:
+        if lead["email"] == email:
+            lead["status"] = new_status
+            DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+            return True
+    return False
+
+#Delete
+def delete_lead(email):
+    leads = read_leads()
+    novos_leads = [lead for lead in leads if lead["email"] != email]
+    if len(novos_leads) == len(leads):
+        return False
+    DB_PATH.write_text(json.dumps(novos_leads, ensure_ascii=False, indent=2), encoding="utf-8")
+    return True
+    
 
